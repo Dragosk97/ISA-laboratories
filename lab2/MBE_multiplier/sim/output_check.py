4fbca73bb390554e2884e73e002fda9e1b513f22
Gaspare7696 <isa17@localhost.localdomain>
2020-12-04 14:32:03 +0100

    FPU-MBE Synthesis

--- lab2/MBE_multiplier/sim/output_check.py
+++ lab2/MBE_multiplier/sim/output_check.py
@@ -1,5 +1,5 @@
 
-ref_file = open("./results_matlab.txt", 'r')
+ref_file = open("../matlab/results_matlab.txt", 'r')
 dut_file = open("./results_modelsim.txt", 'r')
 
 error_flag = 0
@@ -9,14 +9,14 @@ for dut_line in dut_file:
     dut_line = dut_line.strip()
     input_line = ref_file.readline().strip()
     
-    dut_res = int(dut_line)
-    exp_res = int(input_line)
+    #dut_res = int(dut_line,'b')
+    #exp_res = int(input_line,'b')
 
-    if exp_res != dut_res:
+    if input_line != dut_line:
         error_flag = 1
         print(f"Error at line: {line}")
-        print(f"dut result: {dut_res}")
-        print(f"exp result: {exp_res}")
+        print(f"dut result: {dut_line}")
+        print(f"exp result: {input_line}")
         print()
     line += 1
 
