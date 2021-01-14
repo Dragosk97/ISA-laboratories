710e4008c999152fca7306c23aa2fb87868c820b
Dragosk97 <s234611@studenti.polito.it>
2021-01-14 18:36:46 +0100

    forwawrding_unit update

--- lab3/RISC_V/forwarding_unit/forwarding_unit.vhd
+++ lab3/RISC_V/forwarding_unit/forwarding_unit.vhd
@@ -7,6 +7,8 @@ port( rs1_address_idex: IN std_logic_vector(4 downto 0);
       rs2_address_idex: IN std_logic_vector(4 downto 0);
       rd_address_exmem: IN std_logic_vector(4 downto 0);
       rd_address_memwb: IN std_logic_vector(4 downto 0);
+      exmem_fwd_en:IN std_logic;
+      memwb_fwd_en:IN std_logic;
       mux1_fwd, mux2_fwd: OUT std_logic_vector(1 downto 0));
 end forwarding_unit;
 
@@ -14,21 +16,34 @@ architecture behavioural of forwarding_unit is
 begin
     alu_process : process(rs1_address_idex,rs2_address_idex,rd_address_exmem,rd_address_memwb)
     begin
-        
-        if rs1_address_idex = rd_address_exmem then
-            mux1_fwd <= "10";
-        elsif rs1_address_idex = rd_address_memwb then
-            mux1_fwd <= "01";
+        if exmem_fwd_en = '1' then 
+            if rs1_address_idex = rd_address_exmem then
+                mux1_fwd <= "10";
+            else 
+                mux1_fwd <= "00";
+            end if;
+
+            if rs2_address_idex = rd_address_exmem then
+                mux2_fwd <= "10";
+            else
+                mux2_fwd <= "00";
+            end if;
+        elsif memwb_fwd_en = '1' then
+            if rs1_address_idex = rd_address_memwb then
+                mux1_fwd <= "01";
+            else 
+                mux1_fwd <= "00";
+            end if;
+
+            if rs2_address_idex = rd_address_memwb then
+                mux2_fwd <= "01";
+            else
+                mux2_fwd <= "00";
+            end if;
         else
             mux1_fwd <= "00";
+            mux2_fwd <= "00";     
         end if;
-    
-        if rs2_address_idex = rd_address_exmem then
-            mux2_fwd <= "10";
-        elsif rs2_address_idex = rd_address_memwb then
-            mux2_fwd <= "01";
-        else
-            mux2_fwd <= "00";
-        end if;
+        
     end process;
 end behavioural;
