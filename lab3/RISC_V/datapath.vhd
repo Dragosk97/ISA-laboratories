8d42395f43f88efb9a9c2229eec1da6d8b0efd3d
Gaspare7696 <s275336@studenti.polito.it>
2021-01-22 19:06:18 +0100

    datapath

---
+++ lab3/RISC_V/datapath.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity datapath is
+    port ( 
+
+    
+    );
+end datapath;
+
+ARCHITECTURE structural of datapath is
+
+begin
+
+end structural;
