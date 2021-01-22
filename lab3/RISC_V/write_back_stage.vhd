e24e002749b7202967f2347aa7e57698811f3ef8
Gaspare7696 <s275336@studenti.polito.it>
2021-01-22 18:54:14 +0100

    wb modify

--- lab3/RISC_V/write_back_stage.vhd
+++ lab3/RISC_V/write_back_stage.vhd
@@ -5,8 +5,8 @@ USE ieee.numeric_std.all;
 entity write_back_stage is
     port (
         wb_mux_sel : in std_logic;
-        result_memwb : in signed(31 downto 0);
         data_memwb : in signed(31 downto 0);
+        result_memwb : in signed(31 downto 0);
 
         mux_out : out signed(31 downto 0));
 end write_back_stage;
