3e8f5325c8cfa97ebbaed04e86e0faf40353b4dd
alessandro <alessandro.barrera@studenti.polito.it>
2021-01-23 18:06:16 +0100

    Write back interface debug

--- lab3/RISC_V/write_back_stage.vhd
+++ lab3/RISC_V/write_back_stage.vhd
@@ -4,17 +4,17 @@ USE ieee.numeric_std.all;
 
 entity write_back_stage is
     port (
-        wb_mux_sel : in std_logic;
+        wb_mux_sel_memwb : in std_logic;
         data_memwb : in signed(31 downto 0);
         result_memwb : in signed(31 downto 0);
 
-        mux_out : out signed(31 downto 0));
+        rd_data : out signed(31 downto 0));
 end write_back_stage;
 
 architecture behavioral of write_back_stage is
 
 begin
 
-    mux_out <= data_memwb when wb_mux_sel = '0' else result_memwb;
+    rd_data <= data_memwb when wb_mux_sel_memwb = '0' else result_memwb;
 
 end behavioral ;
