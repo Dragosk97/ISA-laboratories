80c054ac306982e735004699c31d7801abded4ea
alessandro <alessandro.barrera@studenti.polito.it>
2021-01-23 17:49:38 +0100

    Memory stage interface debug

--- lab3/RISC_V/memory_stage.vhd
+++ lab3/RISC_V/memory_stage.vhd
@@ -4,14 +4,14 @@ USE ieee.numeric_std.all;
 
 ENTITY memory_stage IS
 	PORT (
-          --input EX/MEM
           clk: IN std_logic;
+          --input EX/MEM
           result_exmem: IN signed(31 downto 0); 
           data_exmem: IN signed(31 downto 0);
           
           rd_address_exmem: IN std_logic_vector(4 downto 0);
           RegWrite_exmem : in std_logic; --useful fwd
-          wb_mux_sel: IN std_logic;
+          wb_mux_sel_exmem: IN std_logic;
 
           --use for memory
           data_mem_in: IN signed(31 downto 0); --out_of_memory
@@ -24,7 +24,7 @@ ENTITY memory_stage IS
           data_memwb: OUT signed(31 downto 0); --out of memory
           result_memwb: OUT signed(31 downto 0); --out of alu
           rd_address_memwb: OUT std_logic_vector(4 downto 0);
-          memwb_mux_sel_out: OUT std_logic;
+          memwb_mux_sel_memwb: OUT std_logic;
           RegWrite_memwb: out std_logic);
 END memory_stage;
 
@@ -42,7 +42,7 @@ BEGIN
         data_memwb <= data_mem_in;
         result_memwb <= result_exmem;
         rd_address_memwb <= rd_address_exmem;
-        memwb_mux_sel_out <= wb_mux_sel;
+        memwb_mux_sel_memwb <= wb_mux_sel_exmem;
         RegWrite_memwb <= RegWrite_exmem;
     END IF;
 END PROCESS;
