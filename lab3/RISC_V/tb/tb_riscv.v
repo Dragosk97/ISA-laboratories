ae3ddd2ae8999fc02084f147ce29273d36e9cfad
Gaspare7696 <s275336@studenti.polito.it>
2021-01-28 18:29:49 +0100

    tb

--- lab3/RISC_V/tb/tb_riscv.v
+++ lab3/RISC_V/tb/tb_riscv.v
@@ -7,6 +7,7 @@ module tb_riscv ();
    wire [31:0] start_address_i;
    wire [31:0] data_mem_in_i;
    wire [31:0] data_mem_out_i;
+   wire [31:0] address_mem_out_i;
    wire MemRead_i;
    wire MemLoad_i;
    wire [31:0] instruction_in_i;
@@ -26,6 +27,19 @@ module tb_riscv ();
 		 .MemLoad(MemLoad_i),
 		 .instruction_in(instruction_in_i),
 		 .instruction_address(instruction_address_i));
+      
+   data_memory DM( .address(.address_mem_out_i),
+                   .data(data_mem_out_i),
+                   .MemWrite(MemLoad_i),
+                   .MemRead(MemRead_i),
+                   .clock(CLK_i),
+                   .cs('1'),
+                   .rst(RST_i),
+                   .Qout(data_mem_in_i));
+
+
+   instruction_memory IM( .address(.instruction_address_i),
+                          .data(.instruction_in_i));
 
 endmodule
 
