b81db65ce251fd000a89df18b127ca5abb0973c1
Gaspare7696 <isa17@localhost.localdomain>
2021-02-18 23:07:15 +0100

    power

---
+++ lab3/RISC_V_abs/sim/work/@r@i@s@c@v_abs/_primary.vhd
@@ -0,0 +1,16 @@
+library verilog;
+use verilog.vl_types.all;
+entity RISCV_abs is
+    port(
+        clk             : in     vl_logic;
+        rst             : in     vl_logic;
+        start_address   : in     vl_logic_vector(31 downto 0);
+        data_mem_in     : in     vl_logic_vector(31 downto 0);
+        data_mem_out    : out    vl_logic_vector(31 downto 0);
+        address_mem_out : out    vl_logic_vector(31 downto 0);
+        MemRead         : out    vl_logic;
+        MemLoad         : out    vl_logic;
+        instruction_in  : in     vl_logic_vector(31 downto 0);
+        instruction_address: out    vl_logic_vector(31 downto 0)
+    );
+end RISCV_abs;
