5e67e4f04ad6171fb1f7d4bb7df6b450c903404b
Gaspare7696 <isa17@localhost.localdomain>
2021-04-14 18:23:20 +0200

    generic versionof pipeline UVM

--- lab4/FPU_mult/src/DUT_generic.sv
+++ lab4/FPU_mult/src/DUT_generic.sv
@@ -27,24 +27,21 @@ module DUT(dut_if.port_in in_inter, dut_if.port_out out_inter, output enum logic
                 end
 
                 WAIT: begin
-					$display("FPU_mult: WAIT");
-                    $display("-- input A: %b, input B: %b; output Z: %b", in_inter.A, in_inter.B, out_inter.data);
 					if(in_inter.valid) begin
                         in_inter.ready <= 0;
                     	out_inter.valid <= 0;
+						A_pipe = in_inter.A;
+						B_pipe = in_inter.B;
                     	state <= PIPEFILL;
 					end
                 end
                 
                 PIPEFILL: begin
-                    $display("FPU_mult: PIPEFILL %d", cnt+1);
-                    $display("-- input A: %b, input B: %b; output Z: %b", in_inter.A, in_inter.B, out_inter.data);
-                    
                     if (cnt < num_pipe - 2) begin
                         state <= PIPEFILL;
                         out_inter.valid <= 0;
                         cnt += 1;
-                    end else
+                    end else begin
                         out_inter.valid <= 1;
                         state <= SEND;
                         cnt = 0;
@@ -52,10 +49,10 @@ module DUT(dut_if.port_in in_inter, dut_if.port_out out_inter, output enum logic
                 end
 
                 SEND: begin
-					$display("FPU_mult: SEND");
-                    $display("-- input A: %b, input B: %b; output Z: %b", in_inter.A, in_inter.B, out_inter.data);
-					$display("dadda_mult: input A = %g, input B = %g, output OUT = %g",$bitstoshortreal(in_inter.A),$bitstoshortreal(in_inter.B),$bitstoshortreal(out_inter.data));
-                    $display("dadda_mult: input A = %b, input B = %b, output OUT = %b",in_inter.A,in_inter.B,out_inter.data);
+					$display("FP_mult: SEND");
+					$display("FP_mult: input A = %g, input B = %g, output OUT = %g",$bitstoshortreal(A_pipe),$bitstoshortreal(B_pipe),$bitstoshortreal(out_inter.data));
+                    $display("FP_mult: input A = %b, input B = %b, output OUT = %b",A_pipe,B_pipe,out_inter.data);
+					
 					if(out_inter.ready) begin
                         out_inter.valid <= 0;
                         in_inter.ready <= 1;				
