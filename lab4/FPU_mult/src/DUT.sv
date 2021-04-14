d823c4b408c2a2160c1d9938990e06564123770a
alessandro <alessandro.barrera@studenti.polito.it>
2021-04-14 12:09:24 +0200

    display directive testing

--- lab4/FPU_mult/src/DUT.sv
+++ lab4/FPU_mult/src/DUT.sv
@@ -25,6 +25,7 @@ module DUT(dut_if.port_in in_inter, dut_if.port_out out_inter, output enum logic
 
                 WAIT_PIPEFILL: begin
 					$display("FPU_mult: WAIT_PIPEFILL");
+                    $display("-- input A: %b, input B: %b; output Z: %b", in_inter.A, in_inter.B, out_inter.data);
 					if(in_inter.valid) begin
                         in_inter.ready <= 0;
                     	out_inter.valid <= 0;
@@ -41,6 +42,7 @@ module DUT(dut_if.port_in in_inter, dut_if.port_out out_inter, output enum logic
 	
                 WAIT: begin
 						$display("FPU_mult: WAIT");
+                        $display("-- input A: %b, input B: %b; output Z: %b", in_inter.A, in_inter.B, out_inter.data);
                        // $display("dadda_mult: input A = %g, input B = %g, output OUT = %g",$bitstoshortreal(A_pipe),$bitstoshortreal(B_pipe),$bitstoshortreal(out_inter.data));
                       //  $display("dadda_mult: input A = %b, input B = %b, output OUT = %b",A_pipe,B_pipe,out_inter.data);
                         out_inter.valid <= 1;
@@ -51,6 +53,10 @@ module DUT(dut_if.port_in in_inter, dut_if.port_out out_inter, output enum logic
 
                 SEND: begin
 					$display("FPU_mult: SEND");
+                    $display("-- input A: %b, input B: %b; output Z: %b", in_inter.A, in_inter.B, out_inter.data);
+                    $display("");
+
+
 					$display("dadda_mult: input A = %g, input B = %g, output OUT = %g",$bitstoshortreal(A_pipe),$bitstoshortreal(B_pipe),$bitstoshortreal(out_inter.data));
                     $display("dadda_mult: input A = %b, input B = %b, output OUT = %b",A_pipe,B_pipe,out_inter.data);
 					A_pipe <= in_inter.A;
